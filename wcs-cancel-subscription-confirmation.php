9a72448cc0bed35c563095d872de9a6797a3e6a3
Joey Ricketts <19918755+jrick1229@users.noreply.github.com>
2018-07-02 11:15:09 -0400

    Update language in wcs-cancel-subscription-confirmation.php
    Just some English changes :)

--- wcs-cancel-subscription-confirmation.php
+++ wcs-cancel-subscription-confirmation.php
@@ -43,7 +43,7 @@ function wcs_cancel_subscription_confirmation() {
 		$script_atts = array(
 			'ajax_url' => admin_url( 'admin-ajax.php' ),
 			'promt_msg' => apply_filters( "wcs_cancel_confirmation_promt_msg", __("Are you sure you want to cancel your subscription?\nIf so, please type the reason why you want to cancel it here:","wcs-cancel-confirmation" ) ) ,
-			'error_msg' => apply_filters( "wcs_cancel_confirmation_error_msg", __("There has been an error when saving the cancelation reason. Please try again.","wcs-cancel-confirmation" ) )
+			'error_msg' => apply_filters( "wcs_cancel_confirmation_error_msg", __("There has been an error when saving the cancellation reason. Please try again.","wcs-cancel-confirmation" ) )
 		);
 		wp_localize_script( 'wcs-cancel-subscription-confirmation-script', 'ajax_object', $script_atts );
 		wp_enqueue_script( 'wcs-cancel-subscription-confirmation-script' );
@@ -58,7 +58,7 @@ function wcs_cancel_confirmation() {
 
 	$subscription = wc_get_order( $subscription_id );
 
-	$note_id = $subscription->add_order_note( apply_filters( "wcs_cancel_confirmation_note_header", __( "Reason to Cancel:", "wcs-cancel-confirmation" ) )."<br /><b><i>".$reason_to_cancel."</i></b>" );
+	$note_id = $subscription->add_order_note( apply_filters( "wcs_cancel_confirmation_note_header", __( "Cancellation Reason:", "wcs-cancel-confirmation" ) )."<br /><b><i>".$reason_to_cancel."</i></b>" );
 
 	$subscription->save();
 
@@ -75,12 +75,12 @@ function add_cancelation_settings( $settings ) {
 
 	$spliced_array = array_splice( $settings, key( $misc_section_end ), 0, array(
 		array(
-			'name'     => __( 'Ask for the cancelation reason', 'wcs-cancel-confirmation' ),
-			'desc'     => __( 'Promt the customer for a cancelation reason', 'wcs-cancel-confirmation' ),
+			'name'     => __( 'Ask for the cancellation reason', 'wcs-cancel-confirmation' ),
+			'desc'     => __( 'Prompt the customer for a cancellation reason', 'wcs-cancel-confirmation' ),
 			'id'       => 'wcs-cancel-confirmation',
 			'default'  => 'no',
 			'type'     => 'checkbox',
-			'desc_tip' =>  __( 'Ask for the cancelation reason when the customer cancels a subscription from My account page. The provided reason will be added as a subscription note in the backend.' ),
+			'desc_tip' =>  __( 'Ask for the cancellation reason when the customer cancels a subscription from the My Account page. The provided reason will be added as a subscription note in the backend.' ),
 		),
 	) );
 
